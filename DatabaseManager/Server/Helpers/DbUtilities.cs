f7cc0a8afccb188144cef5f9938d1bccb6db0d81
Vidar Andresen <vidarandresen@yahoo.com>
2020-02-07 14:50:04 -0600

    Add project files.

---
+++ DatabaseManager/Server/Helpers/DbUtilities.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatabaseManager.Server.Helpers
+{
+    public class DbUtilities
+    {
+        private SqlConnection sqlCn = null;
+        private int _sqlTimeOut;
+
+        public DbUtilities()
+        {
+            _sqlTimeOut = 1000;
+        }
+
+        public void OpenConnection(string connectionString)
+        {
+            sqlCn = new SqlConnection();
+            sqlCn.ConnectionString = connectionString;
+            sqlCn.Open();
+        }
+
+        public void CloseConnection()
+        {
+            sqlCn.Close();
+        }
+    }
+}
