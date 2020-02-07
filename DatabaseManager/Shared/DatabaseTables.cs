538f324dde899fd554039a5b6d2b81e4aa33f3d7
Vidar Andresen <vidarandresen@yahoo.com>
2020-02-07 16:38:02 -0600

    Added skeleton for PPDM to PPDM page

---
+++ DatabaseManager/Shared/DatabaseTables.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseManager.Shared
+{
+    public class DatabaseTables
+    {
+        public static readonly IList<string> Names = new string[]
+        {
+        "WELL_LOG_CURVE_VALUE",
+        "WELL_LOG_CURVE",
+        "STRAT_WELL_SECTION",
+        "STRAT_UNIT",
+        "STRAT_NAME_SET",
+        "WELL",
+        "BUSINESS_ASSOCIATE",
+        "FIELD",
+        "R_WELL_DATUM_TYPE",
+        "R_WELL_STATUS"
+        };
+    }
+}
---
+++ DatabaseManager/Shared/TransferParameters.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseManager.Shared
+{
+    public class TransferParameters
+    {
+        public string SourceDatabase { get; set; }
+        public string SourceDatabaseServer { get; set; }
+        public string SourceDatabaseUser { get; set; }
+        public string SourceDatabasePassword { get; set; }
+        public string TargetDatabase { get; set; }
+        public string TargetDatabaseServer { get; set; }
+        public string TargetDatabaseUser { get; set; }
+        public string TargetDatabasePassword { get; set; }
+        public string TransferQuery { get; set; }
+        public string WellListFile { get; set; }
+        public string QueryType { get; set; }
+        public string Table { get; set; }
+    }
+}
