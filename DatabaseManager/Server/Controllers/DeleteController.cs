243fe94b4f20b4407e6a406da502af6d58ff8b53
Vidar Andresen <vidarandresen@yahoo.com>
2020-02-07 16:24:54 -0600

    Added a skeleton api controller for delete and a database utility class

---
+++ DatabaseManager/Server/Controllers/DeleteController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace DatabaseManager.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DeleteController : ControllerBase
+    {
+        [HttpPost]
+        public string Delete()
+        {
+            string message = "Good ";
+            //if (student == null)
+            //{
+            //    return NotFound();
+            //}
+
+            //_context.Students.Remove(student);
+            //await _context.SaveChangesAsync();
+
+            return message;
+        }
+    }
+}
