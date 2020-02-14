5bca51248a1d222504008d081c8f3f34a4ac6cf0
Vidar Andresen <vidarandresen@yahoo.com>
2020-02-14 16:15:32 -0600

    Completed copy from one ppdm database to another feature

---
+++ DatabaseManager/Shared/CopyTables.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DatabaseManager.Shared
+{
+    public class CopyTables
+    {
+        public static readonly IList<string> Names = new string[]
+        {
+            "WELL","" +
+            "BUSINESS_ASSOCIATE",
+            "FIELD",
+            "R_WELL_DATUM_TYPE",
+            "R_WELL_STATUS",
+            "STRAT_NAME_SET",
+            "STRAT_UNIT",
+            "STRAT_WELL_SECTION",
+            "WELL_LOG_CURVE",
+            "WELL_LOG_CURVE_VALUE"
+        };
+    }
+}
+
+
--- DatabaseManager/Shared/TransferParameters.cs
+++ DatabaseManager/Shared/TransferParameters.cs
@@ -1,16 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace DatabaseManager.Shared
 {
     public class TransferParameters
     {
+        [Required]
         public string SourceDatabase { get; set; }
+        [Required]
         public string SourceDatabaseServer { get; set; }
         public string SourceDatabaseUser { get; set; }
         public string SourceDatabasePassword { get; set; }
+        [Required]
         public string TargetDatabase { get; set; }
+        [Required]
         public string TargetDatabaseServer { get; set; }
         public string TargetDatabaseUser { get; set; }
         public string TargetDatabasePassword { get; set; }
