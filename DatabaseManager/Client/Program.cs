6eab9d22e01c79112b6b352133ec3bf207a95e43
Vidar Andresen <vidarandresen@yahoo.com>
2020-03-26 19:20:56 -0500

    Solution has been upgraded to Webassembly 3.2 preview 2

--- DatabaseManager/Client/Program.cs
+++ DatabaseManager/Client/Program.cs
@@ -1,16 +1,28 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using DatabaseManager.Client.Helpers;
 
 namespace DatabaseManager.Client
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
 
-        public static IWebAssemblyHostBuilder CreateHostBuilder(string[] args) =>
-            BlazorWebAssemblyHost.CreateDefaultBuilder()
-                .UseBlazorStartup<Startup>();
+            builder.Services.AddBaseAddressHttpClient();
+            builder.Services.AddSingleton<SingletonServices>();
+            builder.Services.AddScoped<IHttpService, HttpService>();
+            builder.Services.AddScoped<IDatabaseTransfer, DatabaseTransfer>();
+            builder.Services.AddScoped<IDataSources, DataSources>();
+            builder.Services.AddScoped<IDataModelCreate, DataModelCreate>();
+
+            await builder.Build().RunAsync();
+        }
     }
 }
