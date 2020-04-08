c07917c6c343753e57fdd80bd464797ce319d242
Vidar Andresen <vidarandresen@yahoo.com>
2020-04-08 14:37:06 -0500

    Added option to create the DSM data model

--- DatabaseManager/Server/Controllers/DataModelController.cs
+++ DatabaseManager/Server/Controllers/DataModelController.cs
@@ -9,6 +9,7 @@ using DatabaseManager.Server.Helpers;
 using DatabaseManager.Shared;
 using Microsoft.Azure.Storage;
 using Microsoft.Azure.Storage.File;
+using Microsoft.AspNetCore.Hosting;
 
 namespace DatabaseManager.Server.Controllers
 {
@@ -16,18 +17,72 @@ namespace DatabaseManager.Server.Controllers
     [ApiController]
     public class DataModelController : ControllerBase
     {
+        private readonly IWebHostEnvironment _env;
         private readonly string connectionString;
         private readonly string container = "sources";
 
-        public DataModelController(IConfiguration configuration)
+        public DataModelController(IConfiguration configuration, IWebHostEnvironment env)
         {
             connectionString = configuration.GetConnectionString("AzureStorageConnection");
+            _env = env;
         }
 
         [HttpPost]
-        public async Task<ActionResult<string>> LoadSqlFile(DataModelParameters dmParameters)
+        public async Task<ActionResult<string>> DataModelCreate(DataModelParameters dmParameters)
         {
             if (dmParameters == null) return BadRequest();
+
+            try
+            {
+                ConnectParameters connector = Common.GetConnectParameters(connectionString, container, dmParameters.DataConnector);
+                if (connector == null) return BadRequest();
+                if (dmParameters.ModelOption == "PPDM Model")
+                {
+                    CreatePPDMModel(dmParameters, connector);
+                }
+                else if (dmParameters.ModelOption == "DMS Model")
+                {
+                    CreateDMSModel(connector);
+                }
+                else if (dmParameters.ModelOption == "Stored Procedures")
+                {
+
+                }
+                else
+                {
+
+                }
+            }
+            catch (Exception ex)
+            {
+                return BadRequest();
+            }
+
+            return Ok($"OK");
+        }
+
+        private void CreateDMSModel(ConnectParameters connector)
+        {
+            try
+            {
+                string contentRootPath = _env.ContentRootPath;
+                string sqlFile = contentRootPath + @"\DataBase\DataScienceManagement.sql";
+                string sql = System.IO.File.ReadAllText(sqlFile);
+                DbUtilities dbConn = new DbUtilities();
+                dbConn.OpenConnection(connector);
+                dbConn.SQLExecute(sql);
+                dbConn.CloseConnection();
+            }
+            catch (Exception ex)
+            {
+                Exception error = new Exception("Create DMS Model Error: ", ex);
+                throw error;
+            }
+            
+        }
+
+        private void CreatePPDMModel(DataModelParameters dmParameters, ConnectParameters connector)
+        {
             try
             {
                 CloudStorageAccount account = CloudStorageAccount.Parse(connectionString);
@@ -40,22 +95,18 @@ namespace DatabaseManager.Server.Controllers
                     if (file.Exists())
                     {
                         string sql = file.DownloadTextAsync().Result;
-                        ConnectParameters connector = Common.GetConnectParameters(connectionString, container, dmParameters.DataConnector);
-                        if (connector == null) return BadRequest();
                         DbUtilities dbConn = new DbUtilities();
                         dbConn.OpenConnection(connector);
                         dbConn.SQLExecute(sql);
                         dbConn.CloseConnection();
                     }
                 }
-                
             }
             catch (Exception ex)
             {
-                return BadRequest();
+                Exception error = new Exception("Create PPDM Model Error: ", ex);
+                throw error;
             }
-
-            return Ok($"OK");
         }
     }
 }
--- DatabaseManager/Shared/DataModelParameters.cs
+++ DatabaseManager/Shared/DataModelParameters.cs
@@ -9,5 +9,6 @@ namespace DatabaseManager.Shared
         public string FileName { get; set; }
         public string FileShare { get; set; }
         public string DataConnector { get; set; }
+        public string ModelOption { get; set; }
     }
 }
