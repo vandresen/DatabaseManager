d24c35dd8790ac9782f63b12c9a14763191ec0f3
Vidar Andresen <vidarandresen@yahoo.com>
2020-06-11 19:38:42 -0500

    Added empty placeholder web pages for data qc and predictions. Made numeric wellbore values nullable to fix a crash in wellbore viewer

--- DatabaseManager/Shared/DataIndex/WellBore.cs
+++ DatabaseManager/Shared/DataIndex/WellBore.cs
@@ -8,13 +8,13 @@ namespace DatabaseManager.Shared.DataIndex
     {
         public int Id { get; set; }
         public string Uwi { get; set; }
-        public double Surface_Latitude { get; set; }
-        public double Surface_Longitude { get; set; }
+        public double? Surface_Latitude { get; set; }
+        public double? Surface_Longitude { get; set; }
         public string Well_Name { get; set; }
         public string Operator { get; set; }
         public string Lease_Name { get; set; }
-        public double Final_Td { get; set; }
-        public double Depth_Datum_Elev { get; set; }
+        public double? Final_Td { get; set; }
+        public double? Depth_Datum_Elev { get; set; }
         public string Depth_Datum { get; set; }
         public string Assigned_Field { get; set; }
         public string Current_Status { get; set; }
