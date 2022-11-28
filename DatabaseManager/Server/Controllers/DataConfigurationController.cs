87f658ef77ed0d04876bca4ad02f64ba0e730477
Vidar Andresen <vidarandresen@yahoo.com>
2022-11-28 13:44:09 -0600

    Data access settings
    Created an empty tab page under settings for data access settings. Create a web api to list files from a folder in azure or read the actual file

---
+++ DatabaseManager/Server/Controllers/DataConfigurationController.cs
@@ -0,0 +1,77 @@
+﻿using DatabaseManager.Common.Helpers;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System;
+using Microsoft.AspNetCore.Http;
+using DatabaseManager.Server.Entities;
+using DatabaseManager.Common.Services;
+using Microsoft.Extensions.Configuration;
+using static MudBlazor.CategoryTypes;
+
+namespace DatabaseManager.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DataConfigurationController : ControllerBase
+    {
+        private readonly IFileStorageServiceCommon _fileStorageService;
+        private string _connectionString;
+        protected ResponseDto _response;
+
+        public DataConfigurationController(IFileStorageServiceCommon fileStorageService, IConfiguration configuration)
+        {
+            this._response = new ResponseDto();
+            this._fileStorageService = fileStorageService;
+            _connectionString = configuration.GetConnectionString("AzureStorageConnection");
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<object>> Get(string folder, string name = "")
+        {
+            try
+            {
+                if(folder == null) 
+                {
+                    _response.IsSuccess = false;
+                    _response.ErrorMessages
+                     = new List<string>() { "Folder missing"};
+                }
+                if(_response.IsSuccess) 
+                {
+                    GetStorageAccount();
+                    _fileStorageService.SetConnectionString(_connectionString);
+                    if (string.IsNullOrEmpty(name))
+                    {
+                        List<string> list = await _fileStorageService.ListFiles(folder);
+                        _response.Result = list;
+                    }
+                    else
+                    {
+                        string content = await _fileStorageService.ReadFile(folder, name);
+                        _response.Result = content;
+                    }
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                _response.IsSuccess = false;
+                _response.ErrorMessages
+                     = new List<string>() { ex.ToString() };
+            }
+            return _response;
+        }
+
+        private void GetStorageAccount()
+        {
+            string tmpConnString = Request.Headers["AzureStorageConnection"];
+            if (!string.IsNullOrEmpty(tmpConnString)) _connectionString = tmpConnString;
+            if (string.IsNullOrEmpty(_connectionString))
+            {
+                Exception error = new Exception($"Azure storage key string is not set");
+                throw error;
+            }
+        }
+    }
+}
---
+++ DatabaseManager/Server/Entities/ResponseDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace DatabaseManager.Server.Entities
+{
+    public class ResponseDto
+    {
+        public bool IsSuccess { get; set; } = true;
+        public object Result { get; set; }
+        public string DisplayMessage { get; set; } = "";
+        public List<string> ErrorMessages { get; set; }
+    }
+}
