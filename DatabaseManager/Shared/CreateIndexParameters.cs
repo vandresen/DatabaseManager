a9e4366d4ff0a3606bc099313d121d7065b961a6
Vidar Andresen <vidarandresen@yahoo.com>
2023-09-09 11:04:28 -0500

    Main page serverless
    Improved some Sqlite issues

--- DatabaseManager/Shared/CreateIndexParameters.cs
+++ DatabaseManager/Shared/CreateIndexParameters.cs
@@ -11,5 +11,6 @@ namespace DatabaseManager.Shared
         public string TargetName { get; set; }
         public string Taxonomy { get; set; }
         public string Filter { get; set; }
+        public string Project { get; set; }
     }
 }
