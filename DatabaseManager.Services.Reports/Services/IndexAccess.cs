093afbbe0a0207b894bf158bd00845ef78108ef4
Vidar Andresen <vidarandresen@yahoo.com>
2024-11-13 19:56:30 -0700

    Reports microservice
    More loggings

--- DatabaseManager.Services.Reports/Services/IndexAccess.cs
+++ DatabaseManager.Services.Reports/Services/IndexAccess.cs
@@ -1,15 +1,18 @@
 ﻿using DatabaseManager.Services.Reports.Extensions;
 using DatabaseManager.Services.Reports.Models;
+using Microsoft.Extensions.Logging;
 
 namespace DatabaseManager.Services.Reports.Services
 {
     public class IndexAccess : BaseService, IIndexAccess
     {
         private readonly IHttpClientFactory _clientFactory;
+        private readonly ILogger<IndexAccess> _logger;
 
-        public IndexAccess(IHttpClientFactory clientFactory) : base(clientFactory)
+        public IndexAccess(IHttpClientFactory clientFactory, ILogger<IndexAccess> logger) : base(clientFactory)
         {
             _clientFactory = clientFactory;
+            _logger = logger;
         }
 
         public async Task<T> GetIndexFailures<T>(string dataSource, string project, string dataType, string qcString)
@@ -34,6 +37,7 @@ namespace DatabaseManager.Services.Reports.Services
             {
                 url = SD.IndexAPIBase.BuildFunctionUrl("/api/DmIndexes", $"Name={dataSource}&Node=/&Level=0", SD.IndexKey);
             }
+            _logger.LogInformation($"Url = {url}");
             return await this.SendAsync<T>(new ApiRequest()
             {
                 ApiType = SD.ApiType.GET,
