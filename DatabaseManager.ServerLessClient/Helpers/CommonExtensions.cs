7c5338b7e9aa9340783e44b64c9999019b0e3865
Vidar Andresen <vidarandresen@yahoo.com>
2025-07-08 14:57:14 -0600

    Serverless GUI
    Changed it to use the data source microservice to manage data sources

--- DatabaseManager.ServerLessClient/Helpers/CommonExtensions.cs
+++ DatabaseManager.ServerLessClient/Helpers/CommonExtensions.cs
@@ -1,4 +1,6 @@
-﻿namespace DatabaseManager.ServerLessClient.Helpers
+﻿using DatabaseManager.ServerLessClient.Models;
+
+namespace DatabaseManager.ServerLessClient.Helpers
 {
     public static class CommonExtensions
     {
@@ -24,6 +26,33 @@
             string[] words = attributes.Split(',');
 
             return words;
+        }
+
+        public static string CreateDatabaseConnectionString(this ConnectParameters connection)
+        {
+            string source = $"Source={connection.DatabaseServer};";
+            string database = $"Initial Catalog ={connection.Catalog};";
+            string timeout = "Connection Timeout=120";
+            string persistSecurity = "Persist Security Info=False;";
+            string multipleActive = "MultipleActiveResultSets=True;";
+            string integratedSecurity = "";
+            string user = "";
+            //Encryption is currently not used, more testing later
+            //string encryption = "Encrypt=True;TrustServerCertificate=False;";
+            string encryption = "Encrypt=False;";
+            if (!string.IsNullOrWhiteSpace(connection.User))
+                user = $"User ID={connection.User};";
+            else
+                integratedSecurity = "Integrated Security=True;";
+            string password = "";
+            if (!string.IsNullOrWhiteSpace(connection.Password)) password = $"Password={connection.Password};";
+
+            string cnStr = "Data " + source + persistSecurity + database +
+                user + password + integratedSecurity + encryption + multipleActive;
+
+            cnStr = cnStr + timeout;
+
+            return cnStr;
         }
     }
 }
